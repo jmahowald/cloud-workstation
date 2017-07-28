f14cb7dcebc846912c8813364bd197afee5b5b48
Josh Mahowald (Contractor) <josh.mahowald@code42.com>
2017-07-28 15:18:35 -0500

    updating instructions, completing DEVOPS-78

--- workstation.sh
+++ workstation.sh
@@ -2,9 +2,9 @@
 
 docker run -it --rm \
    -v $(pwd):/workspace \
-   -v ~/.ssh/gitlab_rsa:/root/.ssh/id_rsa:ro \
+   -v ~/.ssh/id_rsa:/root/.ssh/id_rsa:ro \
    -v ~/.ssh/known_hosts:/root/.ssh/known_hosts:ro \
    -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-     joshmahowald/cloud-workstation $@
+     artifactory.corp.code42.com:5000/c42/cloud-workstation $@
