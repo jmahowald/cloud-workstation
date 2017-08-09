9d960994ed789e41804eb3729e8c352958de16d9
Josh Mahowald (Contractor) <josh.mahowald@code42.com>
2017-08-09 16:26:06 -0500

    removed extraneous debugging

--- cloud-workstation.sh
+++ cloud-workstation.sh
@@ -14,6 +14,8 @@ docker run -it --rm \
     -v $(pwd):/workspace \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v ~/.ssh/id_rsa:/root/.ssh/id_rsa:ro \
+    -e DIRENV_ALLOW=$DIRENV_ALLOW \
+    -e AWS_ROLE_NAME=$AWS_ROLE_NAME \
     -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
