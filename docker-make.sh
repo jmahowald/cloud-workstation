b1aba69cffe0328500ecfbba0f5f8db0ee9d4cfe
Josh Mahowald <joshua.mahowald@gmail.com>
2017-12-09 20:50:15 -0600

    stripping down all previous versions

--- docker-make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-docker run -it --rm \
-   -v /var/run/docker.sock:/var/run/docker.sock \
-   -v ~/.docker:/root.docker \
-   -v $(pwd):/usr/src/app \
-   -w /usr/src/app \
-   dmake docker-make $@ 
-
